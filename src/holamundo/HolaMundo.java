2e172c778d9a3f0674c9a46d26803c7b85a9a527
marc <mmarquesayala.com>
2023-03-28 12:33:47 +0200

    first commit

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+
+package holamundo;
+
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("HolaMundo");    
+             
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/holamundo/Programa1.java
@@ -0,0 +1,13 @@
+
+package holamundo;
+
+public class Programa1 {
+    
+public static void main(String[] args) {
+       
+       byte edad = 12; 
+       System.out.println ("La edad es: "+ edad);
+ 
+}
+
+}
